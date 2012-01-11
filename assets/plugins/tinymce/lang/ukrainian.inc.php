246a96a40c07813bd309639c9ad27c121527b262
Max Rogalsky <max.rogalsky@gmail.com>
2012-01-11 16:10:25 +0200

    Ukranian language file for TinyMCE

--- assets/plugins/tinymce/lang/ukrainian.inc.php
+++ assets/plugins/tinymce/lang/ukrainian.inc.php
@@ -1,27 +1,28 @@
 <?php
 /*
- * Filename:       assets/plugins/tinymce/lang/english.inc.php
- * Function:       English language file for TinyMCE
- * Encoding:       ISO-Latin-1
- * Author:         Jeff Whitfield and yama
- * Date:           2010/03/10
+ * Filename:       assets/plugins/tinymce/lang/ukranian.inc.php
+ * Function:       Ukranian language file for TinyMCE
+ * Author:         Max Rogalsky (max.rogalsky@gmail.com)
+ * Date:           2012/01/11
  * Version:        3.3
- * MODx version:   0.9.5-1.0.2
+ * MODx version:   1.0.5
 */
 
-$_lang['tinymce_editor_theme_title'] = 'Theme:';
-$_lang['tinymce_editor_theme_message'] = 'Here you can select which theme or skin to use with the editor.';
-$_lang['tinymce_editor_custom_plugins_title'] = 'Custom Plugins:';
-$_lang['tinymce_editor_custom_plugins_message'] = "Enter the plugins to use for the 'custom' theme as a comma separated list.";
-$_lang['tinymce_editor_custom_buttons_title'] = 'Custom Buttons:';
-$_lang['tinymce_editor_custom_buttons_message'] = "Enter the buttons to use for the 'custom' theme as a comma separated list for each row. Be sure that each button has the required plugin enabled in the 'Custom Plugins' setting.";
-$_lang['tinymce_editor_css_selectors_title'] = 'CSS selectors:';
-$_lang['tinymce_editor_css_selectors_message'] = "Here you can enter a list of selectors that should be available in the editor. Enter them as follows:<br />'displayName=selectorName;displayName2=selectorName2'<br />For instance, say you have <b>.mono</b> and <b>.smallText</b> selectors in your CSS file, you could add them here as:<br />'Monospaced text=mono;Small text=smallText'<br />Note that the last entry should not have a semi-colon after it.";
-$_lang['tinymce_settings'] = 'TinyMCE Settings';
-$_lang['tinymce_theme_simple'] = 'Simple';
-$_lang['tinymce_theme_advanced'] = 'Advanced';
-$_lang['tinymce_theme_editor'] = 'MODx Style';
-$_lang['tinymce_theme_custom'] = 'Custom';
-$_lang['tinymce_theme_creative'] = 'Creative';
+$_lang['tinymce_editor_theme_title'] = "Тема WYSIWYG-редактора:";
+$_lang['tinymce_editor_theme_message'] = "Виберіть яку тему або шаблон ви будете використовувати для WYSIWYG-редактора.";
+$_lang['tinymce_editor_custom_plugins_title'] = "Індивідуальні плагіни:";
+$_lang['tinymce_editor_custom_plugins_message'] = "Введіть, через кому, плагіни для« Індивідуальної »теми.";
+$_lang['tinymce_editor_custom_buttons_title'] = "Індивідуальні кнопки:";
+$_lang['tinymce_editor_custom_buttons_message'] = "Введіть, через кому, кнопки для &laquo;Індивідуальної&raquo; теми. Переконайтеся що додатки, до яких відносяться дані кнопки, прописання в налаштуванні &laquo;Індивідуальні плагіни&raquo;.";
+$_lang['tinymce_editor_css_selectors_title'] = "Селектори CSS:";
+$_lang['tinymce_editor_css_selectors_message'] = "Введіть перелік селекторів, який буде доступний в WYSIWYG-редакторі. Введіть їх наступним чином: <br />&laquo; ім'я, яке буде показано в WYSIWYG-редакторі &laquo;=&raquo; ім'я селектора &laquo;; displayName2 = selectorName2 <br /> Наприклад, у вашому CSS файлі є <b>. mono </ b> та <b>. smallText </ b> селектори, щоб їх додати треба прописати: <br /> 'Монохромний текст = mono; Дрібний текст = smallText '<br /> NB: не ставте крапку з комою (;) після останнього запису. ";
+
+$_lang['tinymce_settings'] = "Налаштування TinyMCE";
+$_lang['tinymce_theme_simple'] = "Проста";
+$_lang['tinymce_theme_advanced'] = "Просунута";
+$_lang['tinymce_theme_editor'] = "Редактор контенту";
+$_lang['tinymce_theme_custom'] = "Індивідуальна";
+$_lang['tinymce_theme_creative'] = 'Творча';
 $_lang['tinymce_theme_logic'] = 'xhtml';
-$_lang['tinymce_theme_global_settings'] = 'Use the global setting';
+$_lang['tinymce_theme_global_settings'] = "Використовувати глобальні налаштування";
+?>
