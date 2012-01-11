e9e3513c5fb6802bba7e9c285031c42fa7ca7b47
Max Rogalsky <max.rogalsky@gmail.com>
2012-01-11 13:50:12 +0200

    Ukranian language file for Ditto

--- assets/snippets/ditto/lang/ukrainian.inc.php
+++ assets/snippets/ditto/lang/ukrainian.inc.php
@@ -1,104 +1,104 @@
 <?php
-
 /*
  * Title: Language File
- * Purpose:
- *  	Default English language file for Ditto
- *  	
- * Note:
- * 		New language keys should added at the bottom of this page
-*/
+ * Purpose: Ukranian language file for Ditto
+ * Note: New language keys should added at the bottom of this page
+ * Translated by:  Max Rogalsky (max.rogalsky@gmail.com)
+ * Date:           11/01/12
+ */
 
-$_lang['language'] = "english";
+$_lang['language'] = "ukranian";
 
-$_lang['abbr_lang'] = "en";
+$_lang['abbr_lang'] = "uk";
 
-$_lang['file_does_not_exist'] = "does not exist. Please check the file.";
+$_lang['file_does_not_exist'] = "не існує. Будь ласка, перевірте файл.";
 
-$_lang['extender_does_not_exist'] = "extender does not exist. Please check it.";
+$_lang['extender_does_not_exist'] = "- дане розширення відсутнє. Будь ласка, перевірте його";
 
 $_lang['default_template'] = <<<TPL
 
     <div class="ditto_item" id="ditto_item_[+id+]">
         <h3 class="ditto_pageTitle"><a href="[~[+id+]~]">[+pagetitle+]</a></h3>
-        <div class="ditto_documentInfo">by <strong>[+author+]</strong> on [+date+]</div>
+        <div class="ditto_documentInfo">Автор: <strong>[+author+]</strong> on [+date+]</div>
         <div class="ditto_introText">[+introtext+]</div>
     </div>
 
 TPL;
 
-$_lang["bad_tpl"] = "<p>&[+tpl+] either does not contain any placeholders or is an invalid chunk name, code block, or filename. Please check it.</p>";
+$_lang["bad_tpl"] = "<p>&[+tpl+] або не містить будь-яких плейсхолдерів, чи є невірним назвою чанка, блоком коду або ім'ям файлу. Будь ласка, перевірте його.</p>';
 
-$_lang['no_documents'] = '<p>No documents found.</p>';
+";
 
-$_lang['resource_array_error'] = 'Resource Array Error';
+$_lang['no_documents'] = '<p>Записів не знайдено.</p>';
+
+$_lang['resource_array_error'] = 'Помилка масиву ресурсів';
  
-$_lang['prev'] = "&lt; Previous";
+$_lang['prev'] = "&lt; назад";
 
-$_lang['next'] = "Next &gt;";
+$_lang['next'] = "далі  &gt;";
 
 $_lang['button_splitter'] = "|";
 
-$_lang['default_copyright'] = "[(site_name)] 2006";
+$_lang['default_copyright'] = "[(site_name)] 2012";
 
-$_lang['invalid_class'] = "The Ditto class is invalid. Please check it.";
+$_lang['invalid_class'] = "Недійсний клас Ditto. Будь ласка, перевірте його.";
 
-$_lang['none'] = "None";
+$_lang['none'] = "Ні";
 
-$_lang['edit'] = "Edit";
+$_lang['edit'] = "Редагувати";
 
 $_lang['dateFormat'] = "%d-%b-%y %H:%M";
 
 // Debug Tab Names
 
-$_lang['info'] = "Info";
+$_lang['info'] = "Інформація";
 
-$_lang['modx'] = "MODx";
+$_lang['modx'] = "MODX";
 
-$_lang['fields'] = "Fields";
+$_lang['fields'] = "Поля";
 
-$_lang['templates'] = "Templates";
+$_lang['templates'] = "Шаблони";
 
-$_lang['filters'] = "Filters";
+$_lang['filters'] = "Фільтри";
 
-$_lang['prefetch_data'] = "Prefetch Data";
+$_lang['prefetch_data'] = "Попередні дані";
 
-$_lang['retrieved_data'] = "Retreived Data";
+$_lang['retrieved_data'] = "Отримані дані";
 
 // Debug Text
 
-$_lang['placeholders'] = "Placeholders";
+$_lang['placeholders'] = "Плейсхолдери"; 
 
-$_lang['params'] = "Parameters";
+$_lang['params'] = "Параметри";
 
-$_lang['basic_info'] = "Basic Info";
+$_lang['basic_info'] = "Основна інформація";
 
-$_lang['document_info'] = "Document Info";
+$_lang['document_info'] = "Інформація про ресурси"
 
-$_lang['debug'] = "Debug";
+$_lang['debug'] = "Налагоджування";
 
-$_lang['version'] = "Version";
+$_lang['version'] = "Версія";
 
-$_lang['summarize'] = "Summarize";
+$_lang['summarize'] = "Число виведених записів (summarize):";
 
-$_lang['total'] = "Total";	 
+$_lang['total'] = "Всього";
 
-$_lang['sortBy'] = "Sort By";
+$_lang['sortBy'] = "Сортувати за:";
 
-$_lang['sortDir'] = "Sort Direction";
+$_lang['sortDir'] = "Порядок сортування:";
 
-$_lang['start'] = "Start";
+$_lang['start'] = "Почати з";
 	 
-$_lang['stop'] = "Stop";
+$_lang['stop'] = "Закінчити";
 
-$_lang['ditto_IDs'] = "IDs";
+$_lang['ditto_IDs'] = "ID";
 
-$_lang['ditto_IDs_selected'] = "Selected IDs";
+$_lang['ditto_IDs_selected'] = "Вибрані ID";
 
-$_lang['ditto_IDs_all'] = "All IDs";
+$_lang['ditto_IDs_all'] = "Всі IDs";
 
-$_lang['open_dbg_console'] = "Open Debug Console";
+$_lang['open_dbg_console'] = "Відкрити консоль налагодження";
 
-$_lang['save_dbg_console'] = "Save Debug Console";
+$_lang['save_dbg_console'] = "Завантажити звіт налагодження";
 
 ?>
